bc649d2d1f38800ef965dfba9d940a00d0b77137
Julian Johannesen <julian.johannesen@gmail.com>
2020-02-06 11:15:09 -0500

    setting up heroku

---
+++ README.md
@@ -0,0 +1,28 @@
+# Library Manager
+
+## What is this?
+
+Library Manager a collection management tool for librarians. It enables librarians to search for, list, add, update, and delete books. 
+
+The application is deployed on Heroku here: 
+
+## Installation
+
+To install library manager, clone the repository and install all dependencies:
+
+```
+git clone git@github.com:TAP-Projects/library-manager.git
+cd library-manager/
+npm clean-install
+```
+
+To start the server run:
+
+```
+npm start
+```
+
+Or start the server in development mode with:
+
+```
+npm run startdev
--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var compression = require('compression');
+var helmet = require('helmet');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,6 +15,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(helmet()); // Set HTTP headers for security
+app.use(compression()); //Compress all routes
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
